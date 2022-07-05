dd3e63da054238e8bff6c28c614d3f66c3ad1cfd
lealinnea <leag0905@gmail.com>
2022-07-04 19:47:07 -0500

    updates to about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,100 @@
 import React from "react";
 import "../styles/Section.css";
-
+import "../styles/About.css";
+import IconOne from "../img/icons/WebIcon1.png";
+import IconTwo from "../img/icons/WebIcon2.png";
+import IconThree from "../img/icons/WebIcon3.png";
+import IconFour from "../img/icons/WebIcon4.png";
+import Gdesign from "../img/graphic design computer image.png";
+import Wdesign from "../img/webdesign computer image.png";
+import GSkills from "../img/Skillset Images.png";
+import WSkills from "../img/Web Development.png";
 
 function Section() {
   return (
     <main>
-      <section className="section">
-        <div class="col">
-          <h1>Hi, I'm Lea Guerrero</h1>
-          <h4>Welcome to my Page. </h4>
-          <p>
-            For as long as I can remember, I’ve had a passion for creativity and
-            design. This led me to attend the Columbus College of Art and
-            Design, and graduate with a degree in Graphic Design. I’m currently
-            employed in a challenging position as a senior graphic designer in a
-            growing and innovative company, where I am responsible for the
-            creation of 2D marketing materials, web page creation and hubspot
-            web maintenance. In addition to corporate experience, I have worked
-            with individuals, small business owners, charitable organizations
-            and political candidates to provide a wide variety of graphics and
-            websites. Web Development has always been a passion of mine. I am
-            currently attending the Northwestern University Coding Bootcamp
-            Program to expand my UI/UX expertise. Click the portfolio link to check
-            out a few if my get hub repo portfolio pieces. Attached to each repo
-            is a readme decribing each project in more detail.
-          </p>
+      <div className="bgc-white">
+        <div className="container padding-tb-60">
+          <div className="row padding-left-60">
+            <div className="col">
+              <h1>Lea Guerrero Designs</h1>
+              <p>
+                Providing creative, original, and attention getting designs for
+                graphics, web sites, video creation, 3D animation and more.
+              </p>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
+      <div className="blockThree">
+      <div className="center bgc-grey">
+          <div className="container ">
+            <div className="container">
+              <div className="row ">
+                <div className="col">
+                  <div className="grey-box">
+                  <img className="icons" src={IconOne} alt="Icon 1" />
+                  <h4 className="text-color-white" >Graphic Design</h4>
+                  <h6 className="text-color-white">10+ Years Experience</h6>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="grey-box">
+                  <img className="icons" src={IconTwo} alt="Icon 2" />
+                  <h4 className="text-color-white" >Web Development</h4>
+                  <h6 className="h6 text-color-white">5+ Years Experience</h6>
+                  </div>
+                </div>
+                <div className="col ">
+                  <div className="grey-box">
+                  <img className="icons" src={IconThree} alt="Icon 3" />
+                  <h4 className="text-color-white" >Video Creation</h4>
+                  <h6 className="text-color-white">3+ Years Experience</h6>
+                </div>
+              </div>
+                <div className="col">
+                  <div className="grey-box">
+                   <img className="icons" src={IconFour} alt="Icon 4" />
+                  <h4 className="text-color-white" >3D Animation</h4>
+                  <h6 className="text-color-white">3+ Years Experience</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+                </div>
+            </div>
+          </div>
+          <div className="blockWhite">
+      <div className=" container padding-top-150 ">
+        <div className="row">
+          <div className="col center padding-bottom-150">
+          <img className="" src={Gdesign} alt="Design Skills" />
+            <a href="./designwork.html"><button className="button">View Portfolio</button></a> 
+          </div>
+          <div className="col">
+            <h1> WHAT I'M GOOD AT</h1>
+            <h4> GRAPHIC DESIGN</h4>
+            <p>Here is a list of my most frequently used design skillsets along with a self assessment of my expertise in each.</p>
+            <img className="Goodat" src={GSkills} alt="Design Skills" />
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row padding-left-60">
+          <div className="col">
+            <h1> WHAT I'M GOOD AT</h1>
+            <h4> WEB DEVELOPMENT</h4>
+            <p>Here is a list of my most frequently used design skillsets along with a self assessment of my expertise in each.</p>
+            <img className="Goodat" src={WSkills} alt="Web Skills" />
+          </div>
+          <div className="center col ">
+          <img className="" src={Wdesign} alt="Web Skills" />
+            <a href=""><button className="button">View Portfolio</button>
+           </a>
+          </div>
+        </div>
+      </div>
+    </div>
     </main>
   );
 }
