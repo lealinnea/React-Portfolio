e3993d8d8611724a1b0864a8e9d92e810bb911a3
lealinnea <leag0905@gmail.com>
2022-07-05 21:47:24 -0500

    Adding to Resume

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import '../styles/Section.css';
-// import Form from "../components/Form"
+import '../styles/Contact.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function Section() {
+function Contact() {
 
   return (
     <main>
-    <section className="section">
-      <h2>Contact Me</h2>
- 
-      {/* <Form /> */}
-    </section>
+      <section className="section">
+
+        <form>
+          <h2>Contact Me</h2>
+          <div className="form-group">
+            <label>Email:</label>
+            <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" />
+          </div>
+          <div className="form-group mt-2">
+            <label>Message:</label>
+            <input className="form-control" id="msg" placeholder="Enter a message" name="msg" />
+          </div>
+          <button type="submit" className="btn btn-success mt-3">Submit</button>
+        </form>
+      </section>
     </main>
   );
 }
 
-export default Section;
+export default Contact;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import '../styles/Resume.css';
 import '../styles/Styles.css';
+import resume from "../ResumeLG.pdf"
 
 function Section() {
 
   return (
     <main>
-    <section className="blockWhite">
-      <h2>Resume</h2> <br></br>
-    </section>
-      <section className="blockWhite"><button className='button center'>Click to Download Resume</button></section>
-    <section className='center blockWhite'>
-   <p>
-     HTLM <br></br>
-     CSS<br></br>
-     Javascript<br></br>
-     Node <br></br>
-     Javascript<br></br>
-     MERN Stack<br></br>
-     MySQL<br></br>
-     Sequelize<br></br>
-     NoSQL <br></br>
-     MongoDB<br></br>
-     Mongoose<br></br>
-   </p>
-   </section>
+      <section className="blockWhite">
+        <h2>Resume</h2> <br></br>
+      </section>
+      <section className="blockWhite">
+        <a href={resume} download="ResumeLG" target='_blank' rel='noreferrer'>
+          <button className='button center'>Download My Resume</button>
+        </a>
+      </section>
+      <section className='center blockWhite'>
+        <p>
+          HTML <br></br>
+          CSS<br></br>
+          Javascript<br></br>
+          Node <br></br>
+          Javascript<br></br>
+          MERN Stack<br></br>
+          MySQL<br></br>
+          Sequelize<br></br>
+          NoSQL <br></br>
+          MongoDB<br></br>
+          Mongoose<br></br>
+        </p>
+      </section>
     </main>
   );
 }
